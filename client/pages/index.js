32c6ff1bf07f7371e9d1e7f4a6fe6664397c3a8e
max-cl <maximiliano.arias@gmail.com>
2021-08-01 15:13:09 +0200

    Client, Check if the token exists

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,6 +1,30 @@
 // Components
 import HomeContainer from "../components/Containers/Home";
 
+// Utils
+import { getAppCookies, verifyToken } from "../util/authUtil";
+
 export default function App() {
     return <HomeContainer />;
+}
+
+export async function getServerSideProps(context) {
+    const { req } = context;
+    const { token } = getAppCookies(req);
+    const profile = token ? verifyToken(token.split(" ")[1]) : "";
+    console.log("profile-Home: ", profile);
+
+    if (!profile) {
+        return {
+            props: {},
+        };
+    } else {
+        console.log("Redirect from Home to Dashboard ");
+        return {
+            redirect: {
+                permanent: false,
+                destination: "/dashboard",
+            },
+        };
+    }
 }
